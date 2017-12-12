9df20158155173ea0d778e39f72fe5c4a94fc8a7
Paul Cernasov <paul.cernasov@gmail.com>
2017-12-12 11:36:10 -0500

    test

---
+++ prepro.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 12 10:54:00 2017
+
+@author: Paul
+"""
+import glob
+import os
+import shutil
+
+def prepro(basedir):
+    print('Hello data is the path '+basedir)
+    
+def main():
+    basedir='/Users/Paul/Desktop/ds000030_R1.0.5_sub10159-10299/data'
+    prepro(basedir)
+    
+main()
